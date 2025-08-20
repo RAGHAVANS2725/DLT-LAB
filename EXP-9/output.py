b25b0136ee4e45a97d21fd3ad298446e59b8b457
RAGHAVANS2725 <raghavans2725@gmail.com>
2025-08-20 08:52:14 +0530

    Add files via upload

---
+++ EXP-9/output.py
@@ -0,0 +1,10 @@
+Python 3.10.9 (tags/v3.10.9:1dd9be6, Dec  6 2022, 20:01:21) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======= RESTART: C:/Users/itsra/OneDrive/Desktop/DLT LAB/EX-9/code.py.py =======
+
+Warning (from warnings module):
+  File "C:\Users\itsra\AppData\Local\Programs\Python\Python310\lib\site-packages\keras\src\layers\core\embedding.py", line 97
+    warnings.warn(
+UserWarning: Argument `input_length` is deprecated. Just remove it.
+Epoch 1/3
